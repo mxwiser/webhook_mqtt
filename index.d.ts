1594493df015b1d8b8680e1e5654acaf098c3348
mxwiser <neoway@126.com>
2025-02-01 07:23:03 +0800

    update ts

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,17 @@
-// types/index.d.ts
+// 
 declare module 'wh2mqt-proxy' {
-    class Webhook_mqtt_wan {
+  export  class Webhook_mqtt_wan {
       constructor(webhookIp: string, webhookPort: number, timeout?: number);
       createBroker( mqttPort: number): void;
       connectMqttServer(mqttHost:string):void;
     }
   
-    class Webhook_mqtt_lan {
+  export  class Webhook_mqtt_lan {
       constructor(mqttIp: string, mqttPort: number);
       createHook(clientId: string, callback: (req: any, res: any) => boolean): void;
-    }
+  }
   
-    export const Webhook_mqtt: {
-      wan: typeof Webhook_mqtt_wan;
-      lan: typeof Webhook_mqtt_lan;
-    };
+
+
 
   }
